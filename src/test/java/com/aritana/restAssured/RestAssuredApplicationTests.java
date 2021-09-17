adf8ba3e91bd9cec40ed0b4da82ebeb1ea43365d
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2021-09-17 18:01:38 -0300

    configuração inicial

---
+++ src/test/java/com/aritana/restAssured/RestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aritana.restAssured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAssuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
