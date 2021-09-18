e163cf7d51956e3848ea4c90675e30331029c255
Aritana Noara <arinoara@yahoo.com.br>
2021-09-17 22:29:02 -0300

    primeiro teste

--- src/main/java/com/aritana/restAssured/TestRestAssured.java
+++ src/main/java/com/aritana/restAssured/TestRestAssured.java
@@ -3,15 +3,28 @@ package com.aritana.restAssured;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
 import javax.swing.SpringLayout;
 
 public class TestRestAssured {
 
+
+
   public static void main(String[] args) {
 
-    Response request = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
-    System.out.println("Opa: ");
-    System.out.println(request.getBody().asString());
+    Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+    System.out.println("----------------------------------------------------------------------------");
+    System.out.println(response.getBody().asString());
+    System.out.println(response.statusCode());
+
+    //testando
+    System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+    System.out.println(response.statusCode()==200);
+
+    ValidatableResponse validation = response.then();
+    validation.statusCode(200);
+
   }
 
 }
