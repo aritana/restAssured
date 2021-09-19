cca12b84cf21795b9a61087778f10127609822c0
Aritana Noara <arinoara@yahoo.com.br>
2021-09-19 11:16:55 -0300

    validacao body

--- src/test/java/com/aritana/restAssured/TestRestAssuredTest.java
+++ src/test/java/com/aritana/restAssured/TestRestAssuredTest.java
@@ -1,20 +1,16 @@
 package com.aritana.restAssured;
 
-import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
-import org.hamcrest.Matcher;
-import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 
@@ -62,6 +58,20 @@ public class TestRestAssuredTest {
         assertThat("Maria",Matchers.not("João"));
         assertThat("Maria",Matchers.allOf(Matchers.startsWith("M"),Matchers.endsWith("ia"), containsString("ar")));
       //  assertThat("Maria", Matchers.anyOf(Matchers.startsWith("M"),Matchers.endsWith("ia"), containsString("ar")));
+    }
+
+    @Test
+    public void devoValidarBody(){
+        given()
+                .when()
+                    .get("https://restapi.wcaquino.me/ola")
+                .then()
+                    .assertThat()//optional
+                    .statusCode(200)
+                    .body(Matchers.is("Ola Mundo!"))
+                    .body(containsString("Mundo"))
+                    .body(is(not(nullValue())));//corpo nao vazio
+
     }
 
 }
