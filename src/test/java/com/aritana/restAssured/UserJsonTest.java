49157bcc18557d4e8bd228418ae0dcd0ab61edd6
Aritana Noara <arinoara@yahoo.com.br>
2021-09-19 20:19:30 -0300

    funcoes especiais

--- src/test/java/com/aritana/restAssured/UserJsonTest.java
+++ src/test/java/com/aritana/restAssured/UserJsonTest.java
@@ -95,4 +95,28 @@ public class UserJsonTest {
                 .statusCode(200)
                 .body("filhos.name", hasItem( Arrays.asList(names)));
     }
+    @Test
+    public void deveFazerVerificacoesAvancadas() {
+
+        given()
+                .when()
+                    .get("https://restapi.wcaquino.me/users")
+                .then()
+                    .statusCode(200)
+                    .body("$", hasSize(3))
+                    .body("age.findAll{it <= 25}.size()",is(2))//tra todos, iterage em todos.
+                    .body("age.findAll{it <= 25 && it > 20}.size()",is(1))
+                    .body("findAll{it.age <= 25 && it.age > 20}.name",hasItem("Maria Joaquina"))
+                    .body("findAll{it.age <= 25}[0].name",is("Maria Joaquina"))
+                    .body("find{it.age <= 25}.name",is("Maria Joaquina"))
+                    .body("findAll{it.name.contains('n')}.name",hasItems("Maria Joaquina","Ana Júlia"))
+                    .body("findAll{it.name.length()>10}.name",hasItems("Maria Joaquina"))
+                    .body("name.collect{it.toUpperCase()}",hasItems("MARIA JOAQUINA"))//Opera o upperCase
+                    .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}",hasItems("MARIA JOAQUINA"))//Opera o upperCase
+                    .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()",allOf(arrayContaining("MARIA JOAQUINA"),arrayWithSize(1)))//Opera o upperCase
+
+        ;
+
+
+    }
 }
