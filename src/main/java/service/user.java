5968cb4e2eece54931b02757a37577b0b0b6126e
Aritana Noara <arinoara@yahoo.com.br>
2021-09-20 18:46:12 -0300

    lombok, classe user'

---
+++ src/main/java/service/user.java
@@ -0,0 +1,17 @@
+package service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class user {
+    private  String name;
+    private Integer age;
+    private Double salary;
+
+}
