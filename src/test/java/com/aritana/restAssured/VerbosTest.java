64e39110426a64eab4928c911657a3986192228d
Aritana Noara <arinoara@yahoo.com.br>
2021-09-20 01:00:17 -0300

    Validação ao salvar

--- src/test/java/com/aritana/restAssured/VerbosTest.java
+++ src/test/java/com/aritana/restAssured/VerbosTest.java
@@ -8,17 +8,33 @@ import static io.restassured.RestAssured.*;
 public class VerbosTest {
 
     @Test
-    public void deveSalvarUsuario(){
+    public void deveSalvarUsuario() {
 
         given()
                 .log().all() //mostra parametros enviados
                 .contentType("application/json")
                 .body("{\"name\":\"Jiraya da Silva\",\"age\":30,\"salary\":1234.5678}")
-        .when()
+                .when()
                 .post("https://restapi.wcaquino.me/users")
-        .then()
-            .log().all()
-            .statusCode(201)
-            .body("id", Matchers.is(Matchers.notNullValue()));
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("id", Matchers.is(Matchers.notNullValue()));
+    }
+
+    @Test
+    public void naoDeveSalvarUsuarioSemNome() {
+        given()
+                .log().all() //mostra parametros enviados
+                .contentType("application/json")
+                .body("{\"age\":30,\"salary\":1234.5678}")
+                .when()
+                .post("https://restapi.wcaquino.me/users")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("id", Matchers.is(Matchers.nullValue()))
+                .body("error", Matchers.is("Name é um atributo obrigatório"));
+
     }
 }
